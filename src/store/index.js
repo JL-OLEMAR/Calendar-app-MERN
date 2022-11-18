d8371c31ddd33e89002a14d678dc836a7e9a3569
JL-OLEMAR <joseluis19963@gmail.com>
2022-11-17 23:23:58 -0500

    feat: Add authSlice

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+export * from './auth/authSlice.js'
 export * from './calendar/calendarSlice.js'
 export * from './ui/uiSlice.js'
 export * from './store.js'
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { calendarSlice, uiSlice } from './'
+import { authSlice, calendarSlice, uiSlice } from './'
 
 export const store = configureStore({
   reducer: {
-    ui: uiSlice.reducer,
-    calendar: calendarSlice.reducer
+    auth: authSlice.reducer,
+    calendar: calendarSlice.reducer,
+    ui: uiSlice.reducer
   },
   // So that it doesn't check, if the dates are serialized.
   middleware: (getDefaultMiddleware) =>
