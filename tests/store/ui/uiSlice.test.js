e8ef94acd1b3f1ba1e8ef39ef5395ecc8b9d2e24
JL-OLEMAR <joseluis19963@gmail.com>
2022-11-23 23:58:30 -0500

    test: Tests in uiSlice

---
+++ tests/store/ui/uiSlice.test.js
@@ -0,0 +1,21 @@
+import {
+  onCloseDateModal,
+  onOpenDateModal,
+  uiSlice
+} from '../../../src/store/ui/uiSlice.js'
+
+describe.only('Tests in uiSlice', () => {
+  test('should return the default state', () => {
+    expect(uiSlice.getInitialState()).toEqual({ isDateModalOpen: false })
+  })
+
+  test('should change the isDateModalOpen correctly', () => {
+    let state = uiSlice.getInitialState()
+
+    state = uiSlice.reducer(state, onOpenDateModal())
+    expect(state.isDateModalOpen).toBeTruthy()
+
+    state = uiSlice.reducer(state, onCloseDateModal())
+    expect(state.isDateModalOpen).toBeFalsy()
+  })
+})
